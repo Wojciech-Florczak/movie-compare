324522f75ac62cf06595146114ecc7b8b6c31b96
wojciech-florczak <florczak@wojciech.dev>
2020-02-02 19:33:51 +0100

    Feat: make autocomplete feature reusable and move it to seperate file

--- index.js
+++ index.js
@@ -1,68 +1,31 @@
-const fetchData = async s => {
-  const res = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "f10e693e",
-      s: s
-    }
-  });
-
-  if (res.data.Error) {
-    return [];
-  }
-
-  return res.data.Search;
-};
-
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-  <label><b>Search for a movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInputChange = async e => {
-  const movies = await fetchData(e.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-
-  resultsWrapper.innerHTML = "";
-  dropdown.classList.add("is-active");
-
-  movies.map(movie => {
-    const option = document.createElement("a");
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+  renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-      `;
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = movie.Title;
-      onMovieSelect(movie);
+    return `
+    <img src="${imgSrc}" />
+    ${movie.Title} (${movie.Year})
+  `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const res = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "f10e693e",
+        s: searchTerm
+      }
     });
 
-    resultsWrapper.appendChild(option);
-  });
-};
+    if (res.data.Error) {
+      return [];
+    }
 
-input.addEventListener("input", debounce(onInputChange, 700));
-
-document.addEventListener("click", e => {
-  if (!root.contains(e.target)) {
-    dropdown.classList.remove("is-active");
+    return res.data.Search;
   }
 });
 
