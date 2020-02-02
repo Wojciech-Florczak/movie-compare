dc69581490cc4de7e45e9614287e1673ef564a20
wojciech-florczak <florczak@wojciech.dev>
2020-02-02 18:22:36 +0100

    Feat: fetch data from movies API, display this data in list, create debounce func for limiting requests

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log('Hello World');
+const fetchData = async s => {
+  const res = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "f10e693e",
+      s: s
+    }
+  });
+
+  if (res.data.Error) {
+    return [];
+  }
+
+  return res.data.Search;
+};
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search for a movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInputChange = async e => {
+  const movies = await fetchData(e.target.value);
+
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+
+  movies.map(movie => {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+
+    resultsWrapper.appendChild(option);
+  });
+};
+
+input.addEventListener("input", debounce(onInputChange, 700));
+
+document.addEventListener("click", e => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
